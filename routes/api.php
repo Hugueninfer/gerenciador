2d4d29e7b92b69d058c0e5d9b193847f7d97a9ff
Hugueninfer <hugueninpedro@gmail.com>
2025-02-17 16:37:32 -0300

    autenticacao baerer token

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
-Route::get('/teste', function () {
-    return response()->json(['mensagem' => 'Teste!']);
-});
+
